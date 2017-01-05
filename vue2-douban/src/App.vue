220e5994947bbde0e344b9f63b446b75263bdc67
235022 <235022@nd.com>
2017-01-05 19:37:47 +0800

    .

--- vue2-douban/src/App.vue
+++ vue2-douban/src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+
+    <div class="wrap">
+      <login></login>
+    </div>
+    <!--广告位-->
+    <div class="ads">
+
+    </div>
+    <div></div>
+
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import Login from './components/login'
+
+require('assets/base.scss')
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Hello,
+    Login
   }
 }
 </script>
@@ -24,5 +36,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.wrap{
+  background:url(https://img3.doubanio.com/f/sns/463f29e6c00cdc1d15226bdcd3c86908526b9380/pics/sns/anony_home/doubanapp4_bg.png) top right no-repeat;
+  background-color: #f1faf1;
 }
 </style>
--- vue2-douban/src/components/login.vue
+++ vue2-douban/src/components/login.vue
@@ -1,38 +1,72 @@
 <template>
-  <form  @submit.prevent="onSubmit">
-    <div><input type="text" id="username" placeholder="邮箱/手机号" tabindex="1" v-model.trim="username"/></div>
-    <div><input type="password" id="password" placeholder="密码" tabindex="2" v-model.trim="password"/></div>
-    <div>
-      <input type="text" id="captcha" placeholder="验证码" tabindex="3" v-model.trim="captcha"/>
-      <img src="https://www.douban.com/misc/captcha?id=OprPkzQI0bhxolO4X4fC62Ua:en&size=s" alt="captcha" id="captcha_img" title="看不清楚?点图片可以换一个"/>
+  <form @submit.prevent="onSubmit" class="login">
+    <div class="mb4">
+      <input type="text" id="username" placeholder="邮箱/手机号" tabindex="1" v-model.trim="username"
+          @focus.in=""                  class="input-txt"/>
     </div>
-    <div>
-      <input type="submit" value="登录豆瓣" tabindex="4" @submit.prevent="onSubmit" />
-      <a href="/accounts/register">注册账号</a>
+    <div class="mb4"><input type="password" id="password" placeholder="密码" tabindex="2" v-model.trim="password" class="input-txt"/>
+    </div>
+    <div class="mb4 pore">
+      <input type="text" id="captcha" placeholder="验证码" tabindex="3" v-model.trim="captcha" class="input-txt"/>
+      <img src="https://www.douban.com/misc/captcha?id=OprPkzQI0bhxolO4X4fC62Ua:en&size=s" alt="captcha"
+           id="captcha_img" title="看不清楚?点图片可以换一个" class="captcha-image"/>
+    </div>
+    <div class="mb4">
+      <input type="submit" value="登录豆瓣" tabindex="4" @submit.prevent="onSubmit" class="btn"/>
+      <a href="/accounts/register" class="btn">注册账号</a>
     </div>
   </form>
 
 </template>
 <style>
+  .login{
+    width: 270px;
+  }
+  .captcha-image{
+    position: absolute;
+    top:3px;
+    width: 158px;
+    height: 26px;
+    right: 0;
+  }
+  .input-txt {
+    width: 252PX;
+    padding: 8px;
+    border: 1px solid #ccc;
+  }
 
 </style>
+
+
 <script>
-   // import HeaderComponent from './components/header.vue'
-    export default{
-        data(){
-            return{
-              username:'',
-              password:'',
-              captcha:''
-            }
-        },
-      methods:{
-        onSubmit(){
-
-        }
+  // import HeaderComponent from './components/header.vue'
+  export default{
+    name: 'login',
+    data () {
+      return {
+        username: '',
+        password: '',
+        captcha: ''
+      }
+    },
+    methods: {
+      onSubmit (evt) {
+          if (this.)
+        console.log(evt)
+      }
+    },
+    computed:{
+      isValid () {
+        return Object.keys(validation).every((key)=> this.validation[key]);
       },
-        components:{
-
+      validation () {
+        return{
+          username: this.name.trim(),
+          password: '',
+          captcha: ''
         }
+      }
+
     }
+  }
 </script>
