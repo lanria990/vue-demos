9371fe3ea6e29557726baa218f95d02cf7a219d6
yjl <990735679@qq.com>
2017-02-05 22:45:03 +0800

    .

---
+++ vue2-douban/src/components/Movie.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="list">
+    <ul>
+      <li v-for="item in items">
+        <a href="{{item.url}}" class="pic">
+          <img src="{{item.src}}" alt="{{item.title}}"/>
+        </a>
+        <a href="{{item.url}}" class="title">{{item.title}}</a>
+        <rating :score="item.score"></rating>
+        <a href="{{item.btnUrl}}" class="btn btn-link">{{item.brnText}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<style lang="sass">
+
+  img {
+    width: 100px;
+  }
+
+  .movie-list .pic {
+    height: 145px;
+    overflow: hidden;
+    display: block;
+  }
+</style>
+
+<script>
+  import  rating from  './Rating'
+  export  default {
+    props: {
+      items: {
+        type: Object,
+        default(){
+          console.error('items must be Object');
+        }
+      },
+      brnText: {
+        type: String,
+        default(){
+              return '选座购票';
+        }
+      }
+    },
+    components: {
+      rating
+    }
+  }
+
+</script>
---
+++ vue2-douban/src/components/Rating.vue
@@ -0,0 +1,27 @@
+<templat>
+  <span class="allstar30"></span>
+  <i>{{score}}</i>
+</templat>
+
+<style>
+
+</style>
+
+<script>
+  export  default{
+      props:{
+          score:{
+              type:Number,
+              default(){
+                  return 0
+              }
+          }
+      },
+    render(){
+          this.className = Math.ceil
+
+
+          (this.score/2)
+    }
+  }
+</script>
